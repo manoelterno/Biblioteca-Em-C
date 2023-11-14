f05b3224d3cab4c6789fd6d194ad3c41f644f71d
manoelterno <149274259+manoelterno@users.noreply.github.com>
2023-11-13 22:10:53 -0300

    Update biblioteca.c
    menu usuarios e decidi utilizar variaveis globais

--- biblioteca.c
+++ biblioteca.c
@@ -2,16 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-void menu()
-{
-    printf("---MENU PRINCIPAL---\n\n");
-    printf("Digite sua escolha\n");
-    printf("1 - Usuario\n");
-    printf("2 - Livro\n");
-    printf("3 - Reserva\n");
-    printf("4 - Relatorios\n");
-    printf("5 - Sair\n\n");
-}
 
 typedef struct data
 {
@@ -52,14 +42,75 @@ typedef struct reserva
     Livro *livro_reservado;
 } Reserva;
 
+//variaveis globais
+Livro livros[20];
+Usuario usuarios[10];
+Reserva reservas[20];
+
+void menu()
+{
+    printf("---MENU PRINCIPAL---\n\n");
+    printf("Digite sua escolha\n");
+    printf("1 - Usuario\n");
+    printf("2 - Livro\n");
+    printf("3 - Reserva\n");
+    printf("4 - Relatorios\n");
+    printf("5 - Sair\n\n");
+}
+
+void menu_usuario()
+{
+    int escolha = 0;
+    while (escolha != 4)
+    {
+        system("cls");
+        printf("---USUARIOS---\n\n");
+        printf("Digite sua escolha\n");
+        printf("1 - Adicionar usuario\n");
+        printf("2 - Editar usuario\n");
+        printf("3 - Excluir usuario\n");
+        printf("4 - Voltar\n\n");
+        scanf("%d", &escolha);
+        switch (escolha)
+        {
+        case 1:
+            //adicionar_usuario();
+            break;
+        case 2:
+            //editar_usuario();
+            break;
+        case 3:
+            //adicionar_usuario();
+            break;
+        }
+    }
+}
+
 int main()
 {
-    int escolha;
-    while(1)
+    int escolha = 0;
+    while (escolha != 5)
     {
         system("cls");
         menu();
         scanf("%d", &escolha);
-    }   
+        switch (escolha)
+        {
+        case 1:
+            menu_usuario();
+            break;
+        case 2:
+            // menu_livro();
+            break;
+        case 3:
+            // menu_usuario();
+            break;
+        case 4:
+            //  menu_relatorio();
+            break;
+        default:
+            break;
+        }
+    }
     return 0;
 }
